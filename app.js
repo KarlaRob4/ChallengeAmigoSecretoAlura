44acf0b17ed40cd4b8934a2efc80e4a10d620178
KarlaRob4 <krobertr1800@alumno.ipn.mx>
2025-09-22 10:22:12 -0600

    actualizacion archivos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let amigo = [];
 let asignacion = [];
+
 function agregarAmigo (){
     let nombre = document.getElementById("amigo").value;
     if (nombre !== ""){
@@ -20,13 +21,20 @@ function actualizarLista(){
 }
 
 function sortearAmigo(){
-    if (nombre.length < 2) {
+    if (amigo.length < 2) {
         return alert("Debe haber al menos dos amigos para realizar el sorteo.");
     } else {
-            index = Math.floor(Math.random() * amigosDisponibles.length);
-            let amigoAsignado = amigosDisponibles[index];
-            asignacion.push({nombre: nombre, amigo: amigoAsignado});
-            amigosDisponibles.splice(index, 1);
-            alert("A " + nombre + " le ha tocado " + amigoAsignado);
+        amigo.forEach(function(amigo) {
+            let amigoAsignado = obtenerAmigoAleatorio(amigo);
+            asignacion.push(amigoAsignado);
+            console.log (amigoAsignado);
+        });
+    }
 }
+
+function obtenerAmigoAleatorio() {
+    let amigoAsignado;  
+    let indiceA = Math.floor(Math.random() * amigo.length);
+    amigoAsignado = amigo[indiceA];
+    return amigoAsignado;
 }
